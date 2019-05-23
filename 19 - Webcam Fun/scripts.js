99d9508c170b57bffa0cf1ce17df22de50312601
duarte-cleiton <duarte.cleiton@outlook.com>
2019-05-23 07:16:35 -0300

    few function left to finish day 19!

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -25,6 +25,13 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height)
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height)
+    // mess with them
+    // pixels = redEffect(pixels)
+    pixels = rgbSplit(pixels)
+    // puth them back
+    ctx.putImageData(pixels, 0, 0)
   }, 16)
 }
 
@@ -39,7 +46,26 @@ function takePhoto() {
   link.href = data
   link.setAttribute("download", "handsome")
   link.textContent = "Download Image"
+  link.innerHTML = `<img src="${data}" alt="Handsome man"/>`
   strip.insertBefore(link, strip.firstChild)
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100 //RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50 //GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5 //BLUE
+  }
+  return pixels
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0] //RED
+    pixels.data[i + 100] = pixels.data[i + 1] //GREEN
+    pixels.data[i - 150] = pixels.data[i + 2] //BLUE
+  }
+  return pixels
 }
 
 getVideo()
