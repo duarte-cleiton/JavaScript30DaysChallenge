14f9ff01d03dd2d4a679047a33173280e1e54aa1
duarte-cleiton <duarte.cleiton@outlook.com>
2019-05-24 06:29:08 -0300

    day 19 done!

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -29,7 +29,9 @@ function paintToCanvas() {
     let pixels = ctx.getImageData(0, 0, width, height)
     // mess with them
     // pixels = redEffect(pixels)
-    pixels = rgbSplit(pixels)
+    // pixels = rgbSplit(pixels)
+    // ctx.globalAlpha = 0.1
+    pixels = greenScreen(pixels)
     // puth them back
     ctx.putImageData(pixels, 0, 0)
   }, 16)
@@ -61,10 +63,38 @@ function redEffect(pixels) {
 
 function rgbSplit(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i - 150] = pixels.data[i + 0] //RED
-    pixels.data[i + 100] = pixels.data[i + 1] //GREEN
-    pixels.data[i - 150] = pixels.data[i + 2] //BLUE
+    pixels.data[i - 550] = pixels.data[i + 0] //RED
+    pixels.data[i + 200] = pixels.data[i + 1] //GREEN
+    pixels.data[i - 500] = pixels.data[i + 2] //BLUE
   }
+  return pixels
+}
+
+function greenScreen(pixels) {
+  const levels = {}
+
+  document.querySelectorAll(".rgb input").forEach(input => {
+    levels[input.name] = input.value
+  })
+
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    red = pixels.data[i + 0]
+    green = pixels.data[i + 1]
+    blue = pixels.data[i + 2]
+    alpha = pixels.data[i + 3]
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      pixels.data[i + 3] = 0
+    }
+  }
+
   return pixels
 }
 
